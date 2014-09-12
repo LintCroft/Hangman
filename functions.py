ff23e15fbe07e15a1e77b508812f301b385cd6be
JamesBrowning1991 <jamesbrowning91@gmail.com>
2014-09-12 11:39:42 +0100

    Code is now commented
    Also made minor changes in save/load functions

--- functions.py
+++ functions.py
@@ -3,58 +3,59 @@ Created on 9 Sep 2014
 
 @author: james.browning
 '''
+#This function checks whether the guessed letter is in the word
 def check_letter(word_list,guess,answer,tries):
-#    global tries
     n = -1
     correct = False
-    
+    #for each letter in the word it checks whether the guessed letter is there
     for letter in word_list:
         if guess == letter:
-            correct = True
+            correct = True #If the guessed letter occurs at least once then the player is correct
             n += 1
-            answer[n] = guess
+            answer[n] = guess #The letter is 'revealed' by adding to the answer list
         else:
             n += 1
-            
+    #function prints a message and then returns the number of tries      
     if correct:
         print("Well Done")
         return tries
     else:
         print("Bad Luck")
-        tries -= 1
+        tries -= 1 #Tries remaining goes down by one if wrong answer
         return tries 
         
-        
+#This function gets a random word from the words.txt file        
 def get_word():
     import random
     word_choice = []
-    with open('words.txt','r') as w:
-        for line in w:
-
+    with open('words.txt','r') as w: #Opens the words.txt file with read permissions
+        for line in w: #for each line it takes the word and puts it in a list
             line = line.rstrip('\n')
             word_choice.append(line)
     
-    choice = random.randint(0,len(word_choice)-1)
-    return word_choice[choice]
+    choice = random.randint(0,len(word_choice)-1) #Now a word is selected at random
+    return word_choice[choice].lower() #Random word is returned
 
-
+#This function simply adds to the score count when the player wins a round
 def scoring(win,score):
     if win:
         score += 1
     return score
 
+# This functions loads a saved game from save.csv, one of two slots is loaded
 def load_on_startup():
     import csv
         
     row_list = []
-    with open('save.csv', 'rt') as csvfile:
-        read_save = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in read_save:
+    with open('save.csv', 'rt') as csvfile: #save.csv file is opened with read permissions
+        read_save = csv.reader(csvfile, delimiter=' ') #delimiter is the separator between fields 
+        for row in read_save: #for each row (there should only be 2) it will add the info into row_list 
             row_list.extend(row)
                     
-    print("Slot 1: {}\nSlot 2: {}".format(row_list[0],row_list[3]))
-    slot = int(input("Which slot would you like to load from, 1 or 2?"))
+    print("Slot 1: {}\nSlot 2: {}".format(row_list[0],row_list[3])) #Prints the names of the previous players saved games
+    slot = int(input("Which slot would you like to load from, 1 or 2?")) #Player chooses a slot to load
     
+    #Relative information is extracted from row_list depending on which slot the player chose
     if slot == 1:
         name = row_list[0]
         score = int(row_list[1])
@@ -64,34 +65,35 @@ def load_on_startup():
         score = int(row_list[4])
         round_count = int(row_list[5])
             
-    return name,score,round_count
+    return name,score,round_count #The extracted information is returned
 
+# This function saves a game into save.csv
 def save_on_exit(score,round_count):
     import csv
-    save = input("Would you like to save your score?\n> ").lower()
+    save = input("Would you like to save your score?\n> ").lower() #PLayer is asked whether they want to save the game
     if save == "y" or save == "yes":
         row_list = []
-        with open('save.csv', 'rt') as csvfile:
-            read_save = csv.reader(csvfile, delimiter=' ', quotechar='|')
-            for row in read_save:
+        with open('save.csv', 'rt') as csvfile: #save.csv is opened with read permissions
+            read_save = csv.reader(csvfile, delimiter=' ') #delimiter is the separator between fields 
+            for row in read_save: #for each row (there should only be 2) it will add the info into row_list 
                 row_list.extend(row)
                 
-        print("Slot 1: {}\nSlot 2: {}".format(row_list[0],row_list[3]))
-        slot = int(input("Which slot would you like to save to, 1 or 2?"))
+        print("Slot 1: {}\nSlot 2: {}".format(row_list[0],row_list[3])) #Prints the names of the previous players saved games
+        slot = int(input("Which slot would you like to save to, 1 or 2?")) #Player chooses a slot to save to
         
-        same_name = input("Would you like to rename the slot?").lower()
+        same_name = input("Would you like to rename the slot?").lower() #Ask player if they wish to rename the save slot
         if same_name == 'y' or same_name == 'yes':
             player_name = input("Please input your name\n> ")
         elif slot == 1:
-            player_name = row_list[0]
+            player_name = row_list[0] #If player doesn't want to rename then player_name is set same as previous save
         elif slot == 2:
             player_name = row_list[3] 
         
-        with open('save.csv', 'wt') as csvfile:
-            write_save = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        with open('save.csv', 'wt') as csvfile: #save.csv is opened with write permissions
+            write_save = csv.writer(csvfile, delimiter=' ') #delimiter is the separator between fields 
             if slot == 1:
-                write_save.writerow([player_name,score, round_count])
-                write_save.writerow([row_list[3],row_list[4],row_list[5]])
+                write_save.writerow([player_name,score, round_count]) #writes player name with current score and round_count
+                write_save.writerow([row_list[3],row_list[4],row_list[5]]) #writes the other slot so data isn't lost
             elif slot == 2:
                 write_save.writerow([row_list[0],row_list[1],row_list[2]])
                 write_save.writerow([player_name,score, round_count])
--- game.py
+++ game.py
@@ -3,79 +3,80 @@ Created on 9 Sept 2014
 
 @author: james.browning
 '''
-
+#Import functions from functions.py
 from functions import check_letter,get_word,scoring,load_on_startup,save_on_exit
 
 
 #Intro message and load game
 print("Welcome to the Hangman game")
 load_game = input("Would you like to load a saved game?\n > ")
-
+#PLayer decides to load a previously saved game, choice of 2 slots
 if load_game == 'y' or load_game == 'yes':
     name,score,round_count = load_on_startup()
-    
+    #Plater is welcomed back, displays previous score and round count
     print("Welcome back {}. Your score is {}/{}".format(name,score,round_count))
+#if player decides not to load then default score and round count are used
 else:
     score = 0
     round_count = 0
 
-used_letters = []
-
-#MainGame, can be restarted
+#MainGame, While can be restarted once round is over
 while True:
-    tries = 10
-    round_count += 1
-    word = get_word()
-    word_list = list(word)
-    answer = []
+    tries = 10 #Number of 'lives' a player has
+    used_letters = [] #Already played letters
+    round_count += 1 #Increment the round_count to aid scoring
+    word = get_word() #Use get_word() function to read words.txt and randomly select a word
+    word_list = list(word) #Converts the word string into a list
+    answer = []#Blank answer list
     
+    #add "_" to the answer list, in place of each character in the word
     for letter in word_list:
         answer.append("_") 
-    
+    #Prints the number of '_' as a string followed by prompt for player to guess a letter
     print(' '.join(answer))
     print("\nGuess a letter")
     guess = input("> ").lower()
-    
+    #Player must guess a single letter otherwise an error is displayed, also not one that has already been tried
     while tries > 0 and "_" in answer and  guess != "give up":
-        if guess.isdigit():
+        if guess.isdigit(): #checks if input is a number
             print("Please use letters only")
             guess = input("> ").lower()
-        elif len(guess) > 1:
+        elif len(guess) > 1: #checks if input is more than 1 character length
             print("Please only guess one letter")
             guess = input("> ").lower()
-        elif len(guess) <1:
+        elif len(guess) <1: #checks if there are no characters input
             print("Please guess a letter")
             guess = input("> ").lower()
-        elif guess in used_letters:
+        elif guess in used_letters: #checks if letter has been guessed before
             print("You have already tried that one")
             guess = input("> ").lower()
-        else:
+        else: #If all is ok it adds the guessed letter to the list of used letters
             used_letters.append(guess)
             tries = check_letter(word_list,guess,answer,tries)
-            if tries > 0 and "_" in answer:
+            if tries > 0 and "_" in answer: #if there are more to guess then the round continues
                 print("\nYou have {} tries remaining".format(tries))
                 print(' '.join(answer))
                 print("\nLetters used: {}".format(' '.join(used_letters)))
                 print("Guess another letter")
                 guess = input("> ").lower()
             else:
-                break
+                break #If there are no more letters to guess, player runs out of tries or types "give up" then the round ends
     
-    if '_' not in answer:
-        print("\nCongratulations")
+    if '_' not in answer: #If there are no more letters to guess then the player has won
+        print("\nYou Win!")
         win = True
         score = scoring(win,score,)
         print ("The answer was'{}'. Your score is {}/{}".format(word,score,round_count))
-    else:
-        print("\nGAME OVER\n")
+    else: #If there are still letters to guess then the player has obviously lost
+        print("\nYou Lose\n")
         win = False
         score = scoring(win,score,)
         print ("The answer was'{}'. Your score is {}/{}".format(word,score,round_count))
-    
+    #Player is asked to play again, loop is re-started
     if input("\nWould you like to play again? ") == 'y':
         print("\n")
         continue
-    else:
+    else: #When game is exited the user can save their progress to one of two slots
         save_on_exit(score,round_count)
-        print("Bye.")
+        print("Bye.\n")
         break
